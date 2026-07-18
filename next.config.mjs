31a27d5cc94abb7ef5e35d0a04c650763150e153
emailzayriontech-ctrl <emailzayriontech@gmail.com>
2026-07-18 11:57:30 +0700

    update next config to ts and add basePath

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/blog',
-};
-
-export default nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/blog",
   typescript: {
     ignoreBuildErrors: true,
   },
